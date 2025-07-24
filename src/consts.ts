ecd66858d8b96eee862405ceac9d555d4e443458
Geri Reid <145468859+Geri-Reid@users.noreply.github.com>
2025-07-24 11:54:38 +0200

    changed heading font

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Geri Reid';
-export const SITE_DESCRIPTION = 'I am an accessibility consultant from London. I help organisations design inclusive digital products that are built to scale.';
+export const SITE_TITLE = 'Geri Reid - Design Systems & Accessibility Consultant, London';
+export const SITE_DESCRIPTION = 'I help organisations design inclusive digital products that are built to scale.';
